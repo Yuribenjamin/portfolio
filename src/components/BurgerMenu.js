738d33d2fee8db14611c0957f53c466599830416
Abraham Ragab <Abrahammoustafa@hotmail.co.uk>
2018-12-28 19:22:37 +0200

    Create state to handle open&close menu

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -3,11 +3,29 @@ import { slide as Menu } from 'react-burger-menu'
 import { Link } from 'react-router-dom'
 
 class BurgerMenu extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+          menuOpen: false
+        }
+      }
+        
+      handleStateChange (state) {
+        this.setState({menuOpen: state.isOpen})  
+      }
+
+      closeMenu () {
+        this.setState({menuOpen: false})
+      }
     render(){
         return(
-            <Menu pageWrapId={ "page-wrap" }>
-                <Link id="home" className="menu-item" to="/portfolio">Home</Link>
-                <Link id="work" className="menu-item" to="/work">Projects : 2018</Link>
+            <Menu 
+            pageWrapId={ "page-wrap" } 
+            isOpen={this.state.menuOpen}
+            onStateChange={(state) => this.handleStateChange(state)}
+            >
+                <Link onClick={() => this.closeMenu()} id="home" className="menu-item" to="/">Home</Link>
+                <Link onClick={() => this.closeMenu()} id="work" className="menu-item" to="/work">Projects : 2018</Link>
             </Menu>
         );
     }
